694cdda591e120423832479a31e440592d11f8ea
zhongjun2 <526521735@qq.com>
2024-02-20 09:30:54 +0800

    Added easy software

---
+++ src/main/java/com/easysoftware/EasysoftwareApplication.java
@@ -0,0 +1,13 @@
+package com.easysoftware;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasysoftwareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EasysoftwareApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/easysoftware/EasysoftwareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.easysoftware;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasysoftwareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
