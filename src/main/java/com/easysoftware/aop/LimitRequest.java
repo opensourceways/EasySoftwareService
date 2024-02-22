dd4d882393f81fcc6ad615ca80ff35d477f0b5f5
kaede10 <sch_na@163.com>
2024-02-22 14:47:36 +0800

    Add LimitRequest

---
+++ src/main/java/com/easysoftware/aop/LimitRequest.java
@@ -0,0 +1,15 @@
+package com.easysoftware.aop;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Documented
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface LimitRequest {
+    int callTime() default 1;
+    int callCount() default 10;
+}
