4cc2ac3bfeb2839de8c60dadfe0930fde52aee10
gouzhonglin <gouzhonglin@huawei.com>
2024-04-11 16:00:09 +0800

    update /operationconfig v2

--- src/main/java/com/easysoftware/infrastructure/operationconfig/gatewayimpl/converter/OperationConfigConverter.java
+++ src/main/java/com/easysoftware/infrastructure/operationconfig/gatewayimpl/converter/OperationConfigConverter.java
@@ -1,8 +1,12 @@
 package com.easysoftware.infrastructure.operationconfig.gatewayimpl.converter;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.BeanUtils;
 
 import com.easysoftware.application.operationconfig.dto.InputOperationConfig;
+import com.easysoftware.application.operationconfig.vo.OperationConfigVo;
 import com.easysoftware.infrastructure.operationconfig.gatewayimpl.dataobject.OperationConfigDO;
 
 public class OperationConfigConverter {
@@ -11,4 +15,21 @@ public class OperationConfigConverter {
         BeanUtils.copyProperties(input, operationConfigDO);
         return operationConfigDO;
     }
+
+    public static List<OperationConfigVo> toVo(List<OperationConfigDO> doList) {
+        List<OperationConfigVo> res = new ArrayList<>();
+        for (OperationConfigDO op : doList) {
+            OperationConfigVo vo = toVo(op);
+            res.add(vo);
+        }
+        return res;
+    }
+
+    public static OperationConfigVo toVo(OperationConfigDO opDo) {
+        OperationConfigVo opVo = new OperationConfigVo();
+        BeanUtils.copyProperties(opDo, opVo);
+        return opVo;
+    }
+
+    
 }
