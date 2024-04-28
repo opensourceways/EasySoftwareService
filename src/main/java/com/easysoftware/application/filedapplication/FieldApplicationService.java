9ab96d308e6f440f6c1394e11d95b2bafa2149ef
gouzhonglin <gouzhonglin@huawei.com>
2024-04-28 11:32:19 +0800

    update /field

--- src/main/java/com/easysoftware/application/filedapplication/FieldApplicationService.java
+++ src/main/java/com/easysoftware/application/filedapplication/FieldApplicationService.java
@@ -2,8 +2,11 @@ package com.easysoftware.application.filedapplication;
 
 import org.springframework.http.ResponseEntity;
 
+import com.easysoftware.application.filedapplication.dto.FieldColumnSearchCondition;
 import com.easysoftware.application.filedapplication.dto.FiledApplicationSerachCondition;
 
 public interface FieldApplicationService {
     ResponseEntity<Object> queryAll(FiledApplicationSerachCondition condition);
+    ResponseEntity<Object> searchColumn(FieldColumnSearchCondition condition);
+
 }
