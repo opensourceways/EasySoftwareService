d8f0b28d778bc17c53571a4fbe02b88fdb800bfd
zwjsec <zhaiwenjiesec@163.com>
2024-05-27 09:44:52 +0800

    删除无用机机接口

--- src/main/java/com/easysoftware/infrastructure/mapper/ExternalOsDOMapper.java
+++
@@ -1,8 +0,0 @@
-package com.easysoftware.infrastructure.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.easysoftware.infrastructure.externalos.gatewayimpl.dataobject.ExternalOsDO;
-
-public interface ExternalOsDOMapper extends BaseMapper<ExternalOsDO> {
-
-}
