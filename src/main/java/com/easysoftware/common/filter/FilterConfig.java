03f2b8a476ae33dbcd3efc271128b12ebc60a5ff
gouzhonglin <gouzhonglin@huawei.com>
2024-05-27 10:41:51 +0800

    license for java src file

--- src/main/java/com/easysoftware/common/filter/FilterConfig.java
+++ src/main/java/com/easysoftware/common/filter/FilterConfig.java
@@ -1,6 +1,16 @@
+/* Copyright (c) 2024 openEuler Community
+ EasySoftware is licensed under the Mulan PSL v2.
+ You can use this software according to the terms and conditions of the Mulan PSL v2.
+ You may obtain a copy of Mulan PSL v2 at:
+     http://license.coscl.org.cn/MulanPSL2
+ THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
+ EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
+ MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
+ See the Mulan PSL v2 for more details.
+*/
+
 package com.easysoftware.common.filter;
 
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
@@ -10,7 +20,6 @@ import org.springframework.web.filter.CharacterEncodingFilter;
 /**
  * 加载所有的filter并指定filter顺序.
  */
-@Slf4j
 @Configuration
 public class FilterConfig {
     /**
