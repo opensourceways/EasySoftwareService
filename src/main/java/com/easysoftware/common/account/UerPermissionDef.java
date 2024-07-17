5b6e062cb08f0df6e9dc2ce2c924c93a2997496f
liulxb <1964023718@qq.com>
2024-07-17 19:14:48 +0800

    #用户权限认证_20240716_7
    代码优化

--- src/main/java/com/easysoftware/common/account/UerPermissionDef.java
+++ src/main/java/com/easysoftware/common/account/UerPermissionDef.java
@@ -1,13 +1,12 @@
 package com.easysoftware.common.account;
 
-
 public class UerPermissionDef {
     /**
-     * 用户权限：easysoftwareread
+     * user permission - easysoftwareread.
      */
     public static final String USER_PERMISSION_READ = "easysoftwareread";
 
-    private void UserPermission() {
+    private UerPermissionDef() {
         throw new RuntimeException("Not supported for instantiation");
     }
 }
