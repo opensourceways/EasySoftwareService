d82916d96e97fc029e5d06ddce93b1a7dbde30d0
liulxb <1964023718@qq.com>
2024-07-18 21:59:37 +0800

    #用户权限认证_20240716_9
    获取cookie

--- src/main/java/com/easysoftware/common/account/UserPermission.java
+++ src/main/java/com/easysoftware/common/account/UserPermission.java
@@ -6,14 +6,17 @@ import com.easysoftware.common.exception.HttpRequestException;
 import com.easysoftware.common.utils.HttpClientUtil;
 import com.easysoftware.common.utils.ObjectMapperUtil;
 import com.fasterxml.jackson.databind.JsonNode;
+import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
 
+import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Objects;
+import java.util.Optional;
 
 @Component
 public class UserPermission {
@@ -37,6 +40,12 @@ public class UserPermission {
     private String permissionApi;
 
     /**
+     * Value injected for the cookie token name.
+     */
+    @Value("${cookie.token.name}")
+    private String cookieTokenName;
+
+    /**
      * Get user permission by user token and manage token.
      * @return Collection of user permissions.
      */
@@ -48,7 +57,8 @@ public class UserPermission {
         String manageToken = getManageToken();
 
         // 使用userToke、manageToken查询用户权限
-        String response = HttpClientUtil.getHttpClient(permissionApi, manageToken, userToken, userToken);
+        Cookie cookie = getCookie(cookieTokenName);
+        String response = HttpClientUtil.getHttpClient(permissionApi, manageToken, userToken, cookie.getValue());
         JsonNode resJson = ObjectMapperUtil.toJsonNode(response);
 
         String resStaus = resJson.get("status").asText();
@@ -100,6 +110,36 @@ public class UserPermission {
         }
 
         return userToken;
+    }
+
+    /**
+     * Get a cookie from the HttpServletRequest.
+     *
+     * @param   cookieName  The name of the cookie to be obtained.
+     * @return  cookie .
+     */
+    private Cookie getCookie(String cookieName) {
+        ServletRequestAttributes servletRequestAttributes =
+                (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
+        if (Objects.isNull(servletRequestAttributes)) {
+            throw new HttpRequestException("http request content error");
+        }
+
+        Cookie[] cookies = servletRequestAttributes.getRequest().getCookies();
+        Cookie cookie = null;
+        if (null != cookies) {
+            // 获取cookie中的token
+            Optional<Cookie> first = Arrays.stream(cookies).filter(c -> cookieName.equals(c.getName()))
+                    .findFirst();
+            if (first.isPresent()) {
+                cookie = first.get();
+            }
+        }
+
+        if (null == cookie) {
+            throw new NotLoginException("user token expired or no login", "", "");
+        }
+        return cookie;
     }
 
 }
