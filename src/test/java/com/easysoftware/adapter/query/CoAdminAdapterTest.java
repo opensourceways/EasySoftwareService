0ad14b3d4467ca5a6156b7fdb4851f1817abdd93
kaede10 <sch_na@163.com>
2024-09-10 11:03:30 +0800

    协作平台user/repos接口增加测试用例

---
+++ src/test/java/com/easysoftware/adapter/query/CoAdminAdapterTest.java
@@ -0,0 +1,72 @@
+/* Copyright (c) 2024 openEuler Community
+ EasySoftware is licensed under the Mulan PSL v2.
+ You can use this software according to the terms and conditions of the Mulan PSL v2.
+ You may obtain a copy of Mulan PSL v2 at:
+     http://license.coscl.org.cn/MulanPSL2
+ THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
+ EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
+ MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
+ See the Mulan PSL v2 for more details.
+*/
+package com.easysoftware.adapter.query;
+
+import static org.mockito.Mockito.when;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.context.WebApplicationContext;
+
+import com.easysoftware.application.collaboration.CoAdminServiceImpl;
+import com.easysoftware.common.account.UerPermissionDef;
+import com.easysoftware.common.account.UserPermission;
+import com.easysoftware.common.entity.ResultVo;
+import com.easysoftware.common.utils.CommonUtil;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class CoAdminAdapterTest {
+    @Autowired
+    private WebApplicationContext webApplicationContext;
+
+    @InjectMocks
+    private CoAdminServiceImpl coAdminService;
+
+    @MockBean
+    private UserPermission userPermission;
+
+    private MockMvc mockMvc;
+
+    @BeforeEach
+    public void setUp() throws Exception {
+        mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
+    }
+
+    @Test
+    void testQueryRepos() throws Exception {
+        performAdminQueryRepos();
+    }
+    
+    private void performAdminQueryRepos() throws Exception {
+        when(userPermission.checkUserPermission(UerPermissionDef.COLLABORATION_PERMISSION_ADMIN.split(","))).thenReturn(true);
+        
+        MultiValueMap<String, String> paramMap = new LinkedMultiValueMap<>();
+        paramMap.add("pageNum", "1");
+        paramMap.add("pageSize", "10");
+        paramMap.add("repo", "jieba,libsvm");
+        paramMap.add("sigName", "ai");
+        paramMap.add("status", "活跃");
+        paramMap.add("versionStatus", "落后版本");
+        paramMap.add("cveStatus", "没有CVE问题");
+        ResultVo res = CommonUtil.executeGet(mockMvc, "/collaboration/admin/user/repos", paramMap);
+        CommonUtil.assertList(res);
+    }
+}
